8efd4c83335299415d5ce41112082eb4a2a5aec8
anisamuema <anisa.muema.school@gmail.com>
2023-09-18 13:01:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+// Code your solutions in this filefor (let index = 0; index < array.length; index++) {
+    function writeCards(namesArray, eventName) {
+        const newArray = []
+        for (let i = 0; i < namesArray.length; i++) {
+        newArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`)
+        }
+        return newArray;
+        
+    }
+
+    function countDown(number) {
+        while(number >= 0){
+            console.log(number--);
+        }
+
+    }
+    
